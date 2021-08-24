9642d0bf8cd82e9c94b75379f3ec5aa4b1e516fc
lea.arturi <lea.arturi@gmail.com>
2021-08-24 15:54:54 -0300

    Upload de foto en AWS

--- controllers/publicationController.js
+++ controllers/publicationController.js
@@ -1,7 +1,35 @@
 const Publication = require('../models/publication');
+const awsUploadImage = require('../utils/aws-upload-image');
+const { v4: uuidv4 } = require('uuid');
 
 const publish = async (file, ctx) => {
-   console.log('Publicando...');
+   const { createReadStream, mimetype } = await file;
+   const extension = mimetype.split('/')[1];
+
+   const filename = `publication/${uuidv4()}.${extension}`;
+   const fileData = createReadStream();
+
+   try {
+      const result = await awsUploadImage(fileData, filename);
+      const publication = new Publication({
+         idUser: ctx.user.id,
+         file: result.Location,
+         typeFile: mimetype.split('/')[0],
+         createAt: Date.now(),
+      });
+      publication.save();
+
+      return {
+         status: true,
+         urlFile: result.Location,
+      };
+   } catch (error) {
+      return {
+         status: null,
+         urlFile: '',
+      };
+   }
+
    return null;
 };
 
